b018785283cd817e8da84e6ee6d51614a5722c26
Santosh Maila <m.santoshkumar1991@gmail.com>
2022-11-15 16:38:28 +0530

    footer code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
+import Footer from './Footer'
+
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
 
     </div>
   );
